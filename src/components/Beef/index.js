22c1a62b804808f1c66c7a6dc910c18673001bf7
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2022-06-30 18:08:36 -0600

    changes to the img css

---
+++ src/components/Beef/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+
+function Beef() {
+
+
+    return (
+        <section  >
+            <div>
+
+            </div>
+
+            <h1>So hard to build, so easy to demolish.</h1>
+        </section>
+
+    )
+}
+
+export default Beef;
